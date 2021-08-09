573b178e7e102ac5e985c31c21b6d351b6a8ccc7
SoHee <73006274+sh0204@users.noreply.github.com>
2021-08-09 11:50:16 +0900

    BOJ

--- Python/1120.py
+++ Python/1120.py
@@ -1,11 +1,11 @@
 a, b = list(input().split())
-min = len(a)
+Min = len(a)
 
 for i in range(len(b)-len(a)+1):
     cnt =0
     for j in range(len(a)):
         if a[j] != b[i+j]:
             cnt+=1
-    if cnt<min:
-        min = cnt
-print(min)
+    if cnt<Min:
+        Min = cnt
+print(Min)
